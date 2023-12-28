42649c2d5a3d16422c246e10e33a87b630ded099
Egemen Nair <nairegemen@gmail.com>
2023-12-28 23:40:12 +0300

    Created: Basic mongoose connection.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,21 @@
-import express, { Request, Response } from "express";
-const app = express();
+// Imports
+import express, { Request, Response, Express } from "express";
+import mongoose from "mongoose";
 
+// Define express app and PORT
+const app: Express = express();
+const PORT = 5000;
+
+// Connect to DB
+const db = mongoose.connect(
+  "mongodb+srv://admin:BwuJeofI0zyZPzZc@flashcardappmern.kctlvfx.mongodb.net/?retryWrites=true&w=majority"
+);
+
+// Routes
 app.get("/", (req: Request, res: Response) => {
   res.send("Hellooooo");
 });
 
-app.listen(5000);
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}!`);
+});
